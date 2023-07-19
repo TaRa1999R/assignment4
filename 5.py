6604eeb267c211e9a9d0ad53872ad0d7808bba7c
TaRa1999R <rezaietara78@gmail.com>
2023-07-20 00:03:51 +0330

    assignment4

--- 5.py
+++ 5.py
@@ -1,23 +1,23 @@
-# print (" 🚫 REMOVE REPEATED SENTENCES FROM A LIST 🚫 ")
-# print (" Enter 'done' to see the result ")
-# print (" Enter numbers as input ")
+print (" 🚫 REMOVE REPEATED SENTENCES FROM A LIST 🚫 ")
+print (" Enter 'done' to see the result ")
+print (" Enter numbers as input ")
 
-# first = []
-# second = []
+first = []
+second = []
 
-# while True :
-#     vurudi = input (" Please enter the input : ")
+while True :
+    vurudi = input (" Please enter the input : ")
 
-#     if vurudi == "done" :
-#         break
+    if vurudi == "done" :
+        break
 
-#     else :
-#         vurudi = int ( vurudi )
-#         first.append ( vurudi )
+    else :
+        vurudi = int ( vurudi )
+        first.append ( vurudi )
 
-# for sentence in first :
-#     if sentence not in second :
-#         second.append ( sentence )
+for sentence in first :
+    if sentence not in second :
+        second.append ( sentence )
 
-# print (" Your entered list is : ", first )
-# print (" New list is :", second)
+print (" Your entered list is : ", first )
+print (" New list is :", second)
